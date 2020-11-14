9bdd6fc819c67bf0e4e284cd27f2f5ac7e369a7d
Nils Martel <nils.martel@graphmasters.net>
2020-11-14 11:41:25 +0100

    Fill Inverted Index

--- inverted-index/src/lib.rs
+++ inverted-index/src/lib.rs
@@ -1,7 +1,48 @@
 #[cfg(test)]
 mod tests {
-    #[test]
+
     fn it_works() {
         assert_eq!(2 + 2, 4);
+    }
+}
+
+use std::collections::HashMap;
+pub type Token = String;
+pub type DocId = String;
+pub type Pos = usize;
+
+#[derive(Clone, Debug)]
+pub struct InvertedIndex {
+    pub data: HashMap<Token, HashMap<DocId, Vec<Pos>>>,
+}
+
+impl InvertedIndex {
+    pub fn new() -> Self {
+        Self {
+            data: HashMap::new(),
+        }
+    }
+
+    pub fn index_doc(&mut self, name: &str, content: &str) {
+        use tokenize::IntoTokens;
+
+        let tokens = content.tokens();
+
+        for (pos, token) in tokens.enumerate() {
+            if let Some(mut posting) = self.data.get_mut(&token) {
+                if let Some(mut pos_list) = posting.get_mut(name) {
+                    pos_list.push(pos);
+                } else {
+                    posting.insert(name.to_string(), vec![pos]);
+                }
+            } else {
+                self.data.insert(token, {
+                    let value = vec![pos];
+                    let mut postings = HashMap::new();
+                    postings.insert(name.to_string(), value);
+                    postings
+                });
+            }
+        }
     }
 }
