0e0714384146f9be77371aa28434f13503f93f76
Nils Martel <nils.martel@graphmasters.net>
2020-11-14 10:59:37 +0100

    Include project files

---
+++ index-table/README.md
@@ -0,0 +1,6 @@
+# Index Table
+
+construct index table (`Vec<(Word, DocId, Position)>`) from a document, where
+Word is a String
+DocId is a unique identifier for a document
+Position is the amount of words coming before this index, in a given document
---
+++ index-table/src/main.rs
@@ -0,0 +1,60 @@
+fn main() {}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn iter_word() {
+        let word = "Hello, World! My name is Nils Martel.";
+        let iter = Words {
+            chars: word.chars(),
+        };
+
+        let collection: Vec<String> = iter.collect();
+
+        assert_eq!(
+            collection,
+            vec!["Hello", "World", "My", "name", "is", "Nils", "Martel"]
+        );
+    }
+}
+
+struct Words<'a> {
+    chars: std::str::Chars<'a>,
+}
+
+impl<'a> Iterator for Words<'a> {
+    type Item = String;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let mut s = String::with_capacity(6);
+
+        fn alpha(c: char) -> bool {
+            c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z'
+        }
+
+        loop {
+            let c = self.chars.next();
+            match c {
+                None => return None,
+                Some(c) if alpha(c) => {
+                    s.push(c);
+                    break;
+                }
+                _ => continue,
+            }
+        }
+
+        loop {
+            let c = self.chars.next();
+            match c {
+                None => break,
+                Some(c) if alpha(c) => s.push(c),
+                _ => break,
+            }
+        }
+
+        return Some(s);
+    }
+}
